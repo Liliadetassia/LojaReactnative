6640e7464586cdfa7412dc1b0cf320998aa5e92a
Liliadetassia <lylyatavares@2gmail.com>
2022-02-08 18:43:04 -0300

    atualizações

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 
 import { StatusBar } from 'expo-status-bar';
-import Routes from 'react';
+import React from 'react';
 import {AppLoading} from 'expo';
 import {useFonts, Anton_400Regular} from '@expo-google-fonts/anton';
 
